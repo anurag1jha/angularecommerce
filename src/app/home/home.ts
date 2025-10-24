f8275e3c46bb05e4c055960c0b3a32ba63441377
anurag <anurag1jha@gmail.com>
2025-10-24 12:36:52 +0530

    home page design

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { Header } from "../shared/layout/header/header";
+import { RouterLink } from "@angular/router";
+
 
 @Component({
   selector: 'app-home',
-  imports: [Header],
+  imports: [RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
