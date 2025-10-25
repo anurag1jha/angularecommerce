bb093c756cd0830d1b147cb126d6af054017ad71
anurag <anurag1jha@gmail.com>
2025-10-25 16:37:15 +0530

    condition based routing in navbar

--- src/app/shared/layout/header/header.ts
+++ src/app/shared/layout/header/header.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -9,6 +9,28 @@ import { RouterLink } from '@angular/router';
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
-export class Header {
+export class Header implements OnInit {
+  user_role: string | null = '';
+  logged_in: boolean = false;
+
+  constructor(private router:Router){}
+
+  ngOnInit(): void { }
+
+  ngDoCheck(){
+    this.user_role = sessionStorage.getItem('role')
+    this.user_role?.toLowerCase();
+    const user_session_id = sessionStorage.getItem('user_session_id');
+    if(user_session_id){
+      this.logged_in = true;
+    }
+  }
+
+  logout(){
+    sessionStorage.removeItem('user_session_id');
+    sessionStorage.removeItem('role');
+    this.router.navigate(['./sign-in']);
+    location.reload();
+  }
 
 }
